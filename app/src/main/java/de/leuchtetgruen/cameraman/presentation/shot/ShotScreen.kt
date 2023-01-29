93394dafbab949f77ab5fc7eb46561d94cc11d89
Hannes <hannes@leuchtetgruen.de>
2023-01-29 18:35:34 +0100

    default image for shot

--- app/src/main/java/de/leuchtetgruen/cameraman/presentation/shot/ShotScreen.kt
+++ app/src/main/java/de/leuchtetgruen/cameraman/presentation/shot/ShotScreen.kt
@@ -1,15 +1,20 @@
 package de.leuchtetgruen.cameraman.presentation.shot
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.skydoves.landscapist.ImageOptions
 import com.skydoves.landscapist.glide.GlideImage
+
+import de.leuchtetgruen.cameraman.R
 
 @Composable
 fun ShotScreen(navController: NavController,
@@ -18,21 +23,29 @@ fun ShotScreen(navController: NavController,
 
     viewModel.navController = navController
     viewModel.loadShot(id.toInt())
-
-    val imageModel = { viewModel.shotDescription?.imageUrl }; // loading a network image using an URL.
+    
     
     Column(        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally) {
 
-        GlideImage(
-            imageModel = imageModel,
-            imageOptions = ImageOptions(
-                contentScale = ContentScale.Crop,
-                alignment = Alignment.Center
-            ),
-            modifier = Modifier.height(300.dp),
-        )
+        if (viewModel.shotDescription?.imageUrl != null) {
+            GlideImage(
+                imageModel = { viewModel.shotDescription?.imageUrl },
+                imageOptions = ImageOptions(
+                    contentScale = ContentScale.Crop,
+                    alignment = Alignment.Center
+                ),
+                modifier = Modifier.height(300.dp),
+            )
+        }
+        else {
+            Image(imageVector = ImageVector.vectorResource(id = R.drawable.default_image_shot),
+                contentDescription = "Placeholder image for shot",
+                modifier = Modifier.height(300.dp), )
+        }
+
+
 
         Spacer(modifier = Modifier.height(30.dp))
 
